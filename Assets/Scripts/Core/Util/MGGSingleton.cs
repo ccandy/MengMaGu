3b9e6e3ba2e113a2755a31a5934693a6e3225db6
ccandy <ccunandy2001@gmail.com>
2023-04-09 15:18:13 -0700

    finish sinSingleton

--- Assets/Scripts/Core/Util/MGGSingleton.cs
+++ Assets/Scripts/Core/Util/MGGSingleton.cs
@@ -3,12 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MGGSingleton<T> : MonoBehaviour
+public class MGGSingleton<T> : MonoBehaviour where T : Component
 {
     private static T _instance;
 
     public T Instance
     {
-        get { return _instance; }
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T> ();
+                if (_instance == null) {
+                    GameObject obj = new GameObject ();
+                    obj.name = typeof(T).Name;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
