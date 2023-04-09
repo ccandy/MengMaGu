9c61bdd1850e28d39a97704bc3a6cb12b1dac49b
ccandy <ccunandy2001@gmail.com>
2023-04-09 15:55:12 -0700

    add player idle state

---
+++ Assets/Scripts/Core/FSM/PlayerStates/PlayerIdleState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : BaseState<PlayerController>
+{
+    // Start is called before the first frame update
+    public override void EnterState(PlayerController controller)
+    {
+        
+    }
+
+    public override void ExitState(PlayerController controller)
+    {
+        
+    }
+
+    public override void OnUpdate(PlayerController controller)
+    {
+        
+    }
+}
