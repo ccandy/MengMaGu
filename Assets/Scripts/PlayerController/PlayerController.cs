6fe661e059403d7ee559c93bbfa1b532b6124b14
ccandy <ccunandy2001@gmail.com>
2023-04-09 16:08:08 -0700

    add player controller

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private readonly PlayerIdleState _playerIdle = new PlayerIdleState();
+    private BaseState<PlayerController> _currentState;
+
+    private Animator _playerAnimator;
+
+    public Animator PlayerAnimator
     {
-        
+        get
+        {
+            return _playerAnimator;
+        }
     }
 
+    void Start()
+    {
+        _playerAnimator = gameObject.GetComponent<Animator>();
+
+        if (_playerAnimator == null)
+        {
+            Debug.LogError("Player has no animator");
+        }
+        
+        TransitionToState(_playerIdle);
+    }
+    
+    
+    public void TransitionToState(BaseState<PlayerController> state)
+    {
+        _currentState = state;
+        _currentState.EnterState(this);
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (_currentState != null)
+        {
+            _currentState.OnUpdate(this);
+        }
     }
 }
