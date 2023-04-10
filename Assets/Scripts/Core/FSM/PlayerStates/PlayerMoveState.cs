d0300f79e2f8a5eff9b68eef8f04f60ad39eb88f
ccandy <ccunandy2001@gmail.com>
2023-04-09 17:54:24 -0700

    finish player movement

--- Assets/Scripts/Core/FSM/PlayerStates/PlayerMoveState.cs
+++ Assets/Scripts/Core/FSM/PlayerStates/PlayerMoveState.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerIdleState : BaseState<PlayerController>
+public class PlayerMoveState : BaseState<PlayerController>
 {
     // Start is called before the first frame update
+
     public override void EnterState(PlayerController controller)
     {
-        controller.PlayerAnimator.SetBool("PlayerWalk", false);
+        Debug.Log("In player move state");
+        controller.PlayerAnimator.SetBool("PlayerWalk", true);
     }
 
     public override void ExitState(PlayerController controller)
@@ -17,16 +19,17 @@ public class PlayerIdleState : BaseState<PlayerController>
 
     public override void OnUpdate(PlayerController controller)
     {
+        
         Vector3 mousePos = Input.mousePosition;
         
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
-            //worldPosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+            
             worldPosition.y = 0;
             worldPosition.z = 0;
             controller.DestPos = worldPosition;
-            controller.TransitionToState(controller.PlayerMoveState);
+            
         }
         
         if (Input.GetMouseButtonDown(1))
@@ -37,6 +40,12 @@ public class PlayerIdleState : BaseState<PlayerController>
         if (Input.GetMouseButtonDown(2))
         {
             Debug.Log("Pressed middle click.");
+        }
+        
+        bool playerMove = controller.PlayerMove();
+        if (!playerMove)
+        {
+            controller.TransitionToState(controller.PlayerIdleState);
         }
     }
 }
--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    private readonly PlayerIdleState _playerIdle = new PlayerIdleState();
+    public readonly PlayerIdleState PlayerIdleState = new PlayerIdleState();
+    public readonly PlayerMoveState PlayerMoveState = new PlayerMoveState();
+    
     private BaseState<PlayerController> _currentState;
 
     private Animator _playerAnimator;
+    private SpriteRenderer _spriteRenderer;
+    private bool _flipX = false;
 
+    public float PlayerMoveSpeed = 1.0f;
+    private Vector3 _destPos = Vector3.zero;
+    private Vector3 _currentDir = Vector3.right;
+    
+    public Vector3 DestPos
+    {
+        set
+        {
+            _destPos = value;
+            CheckFlipX();
+        }
+        get
+        {
+            return _destPos;
+        }
+    }
+    
+    
     public Animator PlayerAnimator
     {
         get
@@ -26,18 +48,45 @@ public class PlayerController : MonoBehaviour
         {
             Debug.LogError("Player has no animator");
         }
-        
-        TransitionToState(_playerIdle);
+
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        if (_spriteRenderer == null)
+        {
+            Debug.LogError("Sprint Render has no animator");
+        }
+        TransitionToState(PlayerIdleState);
     }
-    
+
+    public bool PlayerMove()
+    {   
+        Vector3 currentPos = gameObject.transform.position;
+        currentPos = Vector2.MoveTowards(currentPos, DestPos, PlayerMoveSpeed * Time.deltaTime);
+        gameObject.transform.position = currentPos;
+        return (Vector3.Distance(currentPos, DestPos) >= 0.001f);
+    }
     
     public void TransitionToState(BaseState<PlayerController> state)
     {
         _currentState = state;
         _currentState.EnterState(this);
     }
+
+    private void CheckFlipX()
+    {
+        Vector3 newDir =  _destPos - gameObject.transform.position;
+        float dir = Vector3.Dot(_currentDir, newDir);
+        
+        Debug.Log("Dir is " + dir);
+        
+        if (dir < 0.0f)
+        {
+            _flipX = !_flipX;
+            _spriteRenderer.flipX = _flipX;
+        }
+
+        _currentDir = newDir;
+    }
     
-    // Update is called once per frame
     void Update()
     {
         if (_currentState != null)
