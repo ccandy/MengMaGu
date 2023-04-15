c04faabbe8115a1cecad85d351536d4126a9c03d
ccandy <ccunandy2001@gmail.com>
2023-04-15 13:35:52 -0700

    Update Input manager add player move code

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
-    // Update is called once per frame
-    void Update()
+    private PlayerController _playersController;
+    public void Init(PlayerController controller)
     {
+        _playersController = controller;
+    }
+    public void OnUpdate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
@@ -21,7 +25,25 @@ public class InputManager : MonoBehaviour
                     interactive.OnClickedAction();
                 }
             }
+            else
+            {
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            
+                worldPosition.y = _playersController.gameObject.transform.position.y;
+                worldPosition.z = 0;
+                _playersController.DestPos = worldPosition;
+            }
         }
-        
     }
+
+    public bool PlayerReachDest()
+    {
+        if (_playersController != null)
+        {
+            Debug.LogError("Player is null");
+            return false;
+        }
+        return _playersController.PlayerMove();
+    }
+    
 }
